56407d0c436324c6998f145fef8dfb621856377d
Tyler Pena <tylerpena@me.com>
2021-07-20 22:55:37 -0700

    temp whitelist

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -16,6 +16,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   form: FormGroup;
   loading: boolean;
   subscription = new Subscription();
+  error: boolean;
 
   get publicKey(): FormControl {
     return this.form.get('publicKey') as FormControl;
@@ -43,6 +44,22 @@ export class AuthComponent implements OnInit, OnDestroy {
   }
 
   submit(): void {
+    const listed = [
+      'PAVV2c9Muk9Eu4wi8Fqdmm55ffzhAFPffV',
+      'PHUzrtkLfffDZMd2v8QULRZvBCY5RwrrQK',
+      'PVwyqbwu5CazeACoAMRonaQSyRvTHZvAUh',
+      'PUFLuoW2K4PgJZ4nt5fEUHfvQXyQWKG9hm',
+      'PGZPZpB4iW4LHVEPMKehXfJ6u1yzNPDw7u',
+      'PGD3Rx4wwsXodcMpJr8mnUkkM72mPyMkgu',
+      'PV82zusMGuAQCmv3fbibN18s3kd6YALBTx',
+      'PMGLf8N8QEnKoncvGxcDZYoTNG5ysnxXpX'
+    ];
+
+    if (!listed.includes(this.publicKey.value)) {
+      this.error = true;
+      return;
+    }
+
     this.loading = true;
 
     this._api.auth(environment.marketAddress, this.publicKey.value)
