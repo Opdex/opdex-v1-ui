9f7bdfeef0af01a8aca3a42f10bd2a28fe91608e
Tyler Pena <tylerpena@me.com>
2021-10-16 17:07:29 -0700

    initial pwa commit - wip w/ regards to manifest and site icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,6 +55,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ErrorMiddlewareService } from '@sharedServices/middleware/error-middleware.service';
 import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { environment } from '@environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -113,7 +114,13 @@ import { environment } from '@environments/environment';
       }
     }),
     NgxGoogleAnalyticsModule.forRoot(environment.ga),
-    NgxGoogleAnalyticsRouterModule
+    NgxGoogleAnalyticsRouterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     JwtService,
