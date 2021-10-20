0b9d3206365276ac40731251180752cf91ad62fc
Tyler Pena <tylerpena@me.com>
2021-10-19 17:23:49 -0700

    register service worker immediately

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -123,9 +123,7 @@ import { NotificationsModule } from '@sharedComponents/notifications-module/noti
     NgxGoogleAnalyticsRouterModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })
   ],
   providers: [
