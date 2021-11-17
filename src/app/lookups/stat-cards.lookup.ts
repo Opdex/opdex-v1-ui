8ea989c38011aee2ae83807c97571cce46efe90f
Mark Hypolite <mark.hypolite.mh@gmail.com>
2021-11-16 21:41:00 -0800

    [wapp-108] added Market stat card lookup

--- src/app/lookups/stat-cards.lookup.ts
+++ src/app/lookups/stat-cards.lookup.ts
@@ -1,65 +1,70 @@
-import { IMiningPool } from "@sharedModels/platform-api/responses/liquidity-pools/liquidity-pool.interface";
 import { IMarket } from "@sharedModels/platform-api/responses/markets/market.interface";
 import { StatCardInfo } from "@sharedModels/stat-card-info";
+import { Icons } from "../enums/icons";
 
 export class StatCardLookup {
-  constructor(market?: IMarket, pool?: IMiningPool) {
+  constructor(market?: IMarket) {
     console.log(market);
-    console.log(pool);
+    const info = market;
+    this.setStatCardInfo(info);
   }
 
-  statCardsMarkets: StatCardInfo[] = [
-    {
-      title: 'Liquidity',
-      value: this.market?.summary?.liquidity?.toString(),
-      prefix: '$',
-      change: this.market?.summary?.liquidityDailyChange,
-      show: true,
-      icon: Icons.liquidityPool,
-      iconColor: 'primary',
-      helpInfo: {
-        title: 'What is Liquidity?',
-        paragraph: 'Liquidity represents the total USD amount of tokens locked in liquidity pools through provisioning. Liquidity can be measured for the market as a whole, or at an individual liquidity pool level.'
+  statCardInfo: StatCardInfo[];
+
+  private setStatCardInfo(info: IMarket): void {
+    this.statCardInfo = [
+      {
+        title: 'Liquidity',
+        value: info?.summary?.liquidity?.toString(),
+        prefix: '$',
+        change: info?.summary?.liquidityDailyChange,
+        show: true,
+        icon: Icons.liquidityPool,
+        iconColor: 'primary',
+        helpInfo: {
+          title: 'What is Liquidity?',
+          paragraph: 'Liquidity represents the total USD amount of tokens locked in liquidity pools through provisioning. Liquidity can be measured for the market as a whole, or at an individual liquidity pool level.'
+        }
+      },
+      {
+        title: 'Staking',
+        value: info?.summary?.staking?.weight,
+        suffix: info?.stakingToken?.symbol,
+        change: info?.summary?.staking?.weightDailyChange,
+        show: true,
+        icon: Icons.staking,
+        iconColor: 'stake',
+        helpInfo: {
+          title: 'What is Staking?',
+          paragraph: 'Staking in liquidity pools acts as voting in the mining governance to enable liquidity mining. This indicator displays how many tokens are staking and can be represented for the market as a whole or at an individual staking pool level.'
+        }
+      },
+      {
+        title: 'Volume',
+        value: info?.summary?.volume?.toString(),
+        prefix: '$',
+        daily: true,
+        show: true,
+        icon: Icons.volume,
+        iconColor: 'provide',
+        helpInfo: {
+          title: 'What is Volume?',
+          paragraph: 'Volume is the total USD value of tokens swapped and is usually displayed on a daily time frame. Volume tracks the value of tokens input to the protocol during swaps, plus transaction fees.'
+        }
+      },
+      {
+        title: 'Rewards',
+        value: info?.summary?.rewards?.totalUsd,
+        daily: true,
+        prefix: '$',
+        show: true,
+        icon: Icons.rewards,
+        iconColor: 'reward',
+        helpInfo: {
+          title: 'What are Rewards?',
+          paragraph: 'The rewards indicator displays the total USD value of transaction fees accumulated based on the volume of swap transactions. Rewards are collected by participants for providing liquidity and for staking in active markets.'
+        }
       }
-    },
-    {
-      title: 'Staking',
-      value: this.market?.summary?.staking?.weight,
-      suffix: this.market?.stakingToken?.symbol,
-      change: this.market?.summary?.staking?.weightDailyChange,
-      show: true,
-      icon: Icons.staking,
-      iconColor: 'stake',
-      helpInfo: {
-        title: 'What is Staking?',
-        paragraph: 'Staking in liquidity pools acts as voting in the mining governance to enable liquidity mining. This indicator displays how many tokens are staking and can be represented for the market as a whole or at an individual staking pool level.'
-      }
-    },
-    {
-      title: 'Volume',
-      value: this.market?.summary?.volume?.toString(),
-      prefix: '$',
-      daily: true,
-      show: true,
-      icon: Icons.volume,
-      iconColor: 'provide',
-      helpInfo: {
-        title: 'What is Volume?',
-        paragraph: 'Volume is the total USD value of tokens swapped and is usually displayed on a daily time frame. Volume tracks the value of tokens input to the protocol during swaps, plus transaction fees.'
-      }
-    },
-    {
-      title: 'Rewards',
-      value: this.market?.summary?.rewards?.totalUsd,
-      daily: true,
-      prefix: '$',
-      show: true,
-      icon: Icons.rewards,
-      iconColor: 'reward',
-      helpInfo: {
-        title: 'What are Rewards?',
-        paragraph: 'The rewards indicator displays the total USD value of transaction fees accumulated based on the volume of swap transactions. Rewards are collected by participants for providing liquidity and for staking in active markets.'
-      }
-    }
-  ];
+    ];
+  }
 }
