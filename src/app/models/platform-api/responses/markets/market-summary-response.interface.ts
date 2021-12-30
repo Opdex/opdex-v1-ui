81d89b2cdeff8bbaf87f792a28bc03b9b4a7ec71
Tyler Pena <tylerpena@me.com>
2021-12-30 13:03:12 -0800

    market summary response model changes

--- src/app/models/platform-api/responses/markets/market-summary-response.interface.ts
+++ src/app/models/platform-api/responses/markets/market-summary-response.interface.ts
@@ -2,9 +2,13 @@ export interface IMarketSummaryResponse {
   liquidityUsd: number;
   dailyLiquidityUsdChangePercent: number;
   volumeUsd: number;
+  staking: IMarketStakingResponse;
+  rewards: any;
+}
+
+export interface IMarketStakingResponse {
   stakingWeight: string;
   dailyStakingWeightChangePercent: number;
   stakingUsd: number;
   dailyStakingUsdChangePercent: number;
-  rewards: any;
 }
