14c0e2972600ee88489ef11d905a7e0e5eb17043
Tyler Pena <tylerpena@me.com>
2022-01-19 10:45:55 -0800

    v1 api version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { Network } from 'src/app/enums/networks';
 
-// const api = 'http://localhost:44391';
-const api = 'https://v1-dev-api.opdex.com';
+// const api = 'http://localhost:44391/v1';
+const api = 'https://v1-dev-api.opdex.com/v1';
 
 export const environment = {
   production: false,
